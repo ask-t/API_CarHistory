8cf79684700f2d1dea4b6676e02b46fc7e0b58f3
ask-t <asktakahashi@go.byuh.edu>
2024-03-29 08:20:12 -1000

    add new Schema

--- src/mileage/mileage.module.ts
+++ src/mileage/mileage.module.ts
@@ -2,11 +2,13 @@ import { Module } from '@nestjs/common';
 import { MileageService } from './mileage.service';
 import { MileageController } from './mileage.controller';
 import { MileageSchema } from './schemas/mileage.schema';
+import { GasIDSchema } from './schemas/gasid.schema';
 import { MongooseModule } from '@nestjs/mongoose';
 
 @Module({
   imports: [
     MongooseModule.forFeature([{ name: 'Mileage', schema: MileageSchema }]),
+    MongooseModule.forFeature([{ name: 'GasID', schema: GasIDSchema }]),
   ],
   controllers: [MileageController],
   providers: [MileageService],
